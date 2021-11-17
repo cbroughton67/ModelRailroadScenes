9679623f3a9195a8af7354aec942258b226461cf
Chris Broughton <cbroughton67@gmail.com>
2021-11-16 20:48:52 -0500

    about page, footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* function to update copyright date on page load */
+function copyrightDate() {
+    const d = new Date();
+    document.getElementById("copyrightDate").innerHTML = d.getFullYear();
+  }
+  
+  function myFunction() {
+    alert("Alert!!!");
+  }
+  
