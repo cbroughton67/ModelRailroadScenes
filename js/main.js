a9b21128318b20cd21e15c49deb62969f76f4f67
Chris Broughton <cbroughton67@gmail.com>
2021-11-22 22:50:59 -0500

    added video player, more content

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-/* function to update copyright date on page load */
+/* function to update copyright date */
 function copyrightDate() {
-    const d = new Date();
-    document.getElementById("copyrightDate").innerHTML = d.getFullYear();
+    const cdate = new Date();
+    document.getElementById("copyrightDate").innerHTML = cdate.getFullYear();
   }
   
+  /* function used for testing */
   function myFunction() {
     alert("Alert!!!");
   }
   
+  function rickRoll() {
+    document.getElementById("easteregg").style.display='block';
+    
+  }
